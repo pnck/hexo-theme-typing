bff2ca4ed131b898df01cb4fbde18036e2302e88
pnck <hio131@gmail.com>
2022-03-03 12:47:41 +0800

    move decrypt script

---
+++ scripts/encrypt_inplace.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const { log, route } = hexo;
+const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
+
+
+function arrayBufferToHex(arrayBuffer) {
+    if (typeof arrayBuffer !== 'object' || arrayBuffer === null || typeof arrayBuffer.byteLength !== 'number') {
+        throw new TypeError('Expected input to be an ArrayBuffer')
+    }
+
+    let view = new Uint8Array(arrayBuffer)
+    let result = ''
+    let value
+
+    for (let i = 0; i < view.length; i++) {
+        value = view[i].toString(16)
+        result += (value.length === 1 ? '0' + value : value)
+    }
+
+    return result
+}
+
+
+hexo.extend.generator.register('custom-encrypt', () => [
+    {
+        'data': () => fs.createReadStream(path.resolve(__dirname, '../source/js/__hash_decrypt.js')),
+        'path': '/js/__hash_decrypt.js',
+    },
+]);
+
+hexo.extend.filter.register('after_post_render', (data) => {
+    if (data.encrypt) {
+        const password = data.encrypt_pwd || "show";
+        log.debug('encrypt article >> ', data.title);
+        const key = crypto.pbkdf2Sync(password, "ksalt", 1024, 32, 'sha256');
+        const iv = crypto.pbkdf2Sync(password, "ivsalt", 512, 12, 'sha256');
+        const cipher = crypto.createCipheriv('aes-256-gcm', key, iv);
+        log.debug('key=', arrayBufferToHex(key));
+        log.debug('iv=', arrayBufferToHex(iv));
+        // cipher.setAutoPadding(true);
+        let encrypted = cipher.update(data.content, 'utf8', 'hex');
+        encrypted += cipher.final('hex');
+        encrypted += arrayBufferToHex(cipher.getAuthTag());
+        data.content = `<div id="encrypted-content">${encrypted}</div>`;
+        data.content += `<script data-pjax src="${hexo.config.root}js/__hash_decrypt.js"></script>`
+    }
+    return data;
+});
---
+++ source/js/__hash_decrypt.js
@@ -0,0 +1,82 @@
+(() => {
+    'use strict';
+
+    const crypto = window.crypto || window.msCrypto;
+
+    const encoder = new TextEncoder();
+
+    const keySalt = encoder.encode('ksalt');
+    const ivSalt = encoder.encode('ivsalt');
+
+    const encryptedElement = document.getElementById('encrypted-content');
+    if(!encryptedElement){
+        return;
+    }
+    const encryptedContent = encryptedElement.innerText;
+
+    function hexToArray(s) {
+        return new Uint8Array(s.match(/[\da-f]{2}/gi).map((h => {
+            return parseInt(h, 16);
+        })));
+    }
+
+    function getKeyMaterial(password) {
+        return crypto.subtle.importKey(
+            'raw',
+            encoder.encode(password), {
+            name: 'PBKDF2',
+        }, false, ['deriveKey', 'deriveBits']);
+    }
+
+    function getKey(keyMaterial) {
+        return crypto.subtle.deriveKey({
+            name: 'PBKDF2',
+            hash: 'SHA-256',
+            salt: keySalt.buffer,
+            iterations: 1024,
+        }, keyMaterial, {
+            name: 'AES-GCM',
+            length: 256,
+        }, true, [
+            'decrypt',
+        ]);
+    }
+
+    function getIv(keyMaterial) {
+        return crypto.subtle.deriveBits({
+            name: 'PBKDF2',
+            hash: 'SHA-256',
+            salt: ivSalt.buffer,
+            iterations: 512,
+        }, keyMaterial, 12 * 8);
+    }
+
+    function decrypt() {
+        const password = window.location.hash.slice(1);
+        let contentArray = hexToArray(encryptedContent);
+
+        let doDecrypt = async () => {
+            const keyMaterial = await getKeyMaterial(password);
+            const key = await getKey(keyMaterial);
+            const iv = await getIv(keyMaterial);
+
+            // console.log('key=', arrayBufferToHex(await crypto.subtle.exportKey('raw', key)));
+            // console.log('iv=', arrayBufferToHex(iv));
+
+
+            const result = await crypto.subtle.decrypt({
+                name: 'AES-GCM',
+                iv: iv,
+            }, key, contentArray);
+
+            return result;
+        }
+
+        doDecrypt().then((result) => {
+            const content = new TextDecoder().decode(result);
+            encryptedElement.parentElement.innerHTML = content;
+        }).catch(e => { /* suppress *//*console.log(e.name)*/ });
+    }
+    window.onhashchange = decrypt;
+    decrypt();
+})();
