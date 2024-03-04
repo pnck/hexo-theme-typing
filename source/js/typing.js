91ac2e2f7f9ae1bce30cec37c0aea7821dc11575
pnck <pnckxx@pm.me>
2024-03-04 12:29:56 +0800

    feat: styles & animations

--- source/js/typing.js
+++ source/js/typing.js
@@ -1,79 +1,129 @@
 (function ($) {
   // Caption
-  $('.article-entry').each(function (i) {
-    $(this).find('img').each(function () {
-      if ($(this).parent().hasClass('fancybox')) return
+  $(".article-entry").each(function (i) {
+    $(this)
+      .find("img")
+      .each(function () {
+        if ($(this).parent().hasClass("fancybox")) return;
 
-      var alt = this.alt
+        var alt = this.alt;
 
-      if (alt) {
-        $(this).after('<span class="caption">' + alt + '</span>')
-      }
+        if (alt) {
+          $(this).after('<span class="caption">' + alt + "</span>");
+        }
 
-      $(this).wrap('<a href="' + this.src + '" data-fancybox=\"gallery\" data-caption="' + alt + '"></a>')
-    })
+        $(this).wrap(
+          '<a href="' +
+            this.src +
+            '" data-fancybox="gallery" data-caption="' +
+            alt +
+            '"></a>'
+        );
+      });
 
-    $(this).find('.fancybox').each(function () {
-      $(this).attr('rel', 'article' + i)
-    })
-  })
+    $(this)
+      .find(".fancybox")
+      .each(function () {
+        $(this).attr("rel", "article" + i);
+      });
+  });
 
   if ($.fancybox) {
-    $('.fancybox').fancybox()
+    $(".fancybox").fancybox();
   }
 
+  // ---------  fold all when content loaded  ---------
+  const doFold = () => {
+    $(".foldable").next(".fold-content").hide();
+    $(".foldable").off("click");
+    $(".foldable").on("click", function () {
+      if ($(this).hasClass("folded")) {
+        $(this).addClass("expanded");
+        $(this).removeClass("folded");
+        $(this).next(".fold-content").slideDown(200);
+      } else if ($(this).hasClass("expanded")) {
+        $(this).addClass("folded");
+        $(this).removeClass("expanded");
+        $(this).next(".fold-content").slideUp(200);
+      }
+    });
+  };
 
-  $(document).ready(function() {
-	  var QRBox	=	$('#QRBox');
-	  var MainBox	=	$('.MainBox');
-	  var BTCQR	=	$('#BTCQR');
-	  var AliPayQR	=	$('#AliPayQR');
-	  var WeChatQR	=	$('#WeChatQR');
+  // actions when page loaded
+  const onLoaded = () => {
+    setTimeout(() => {
+      $(".home-body").removeClass("loading");
+    }, 100);
+    doFold();
+    // load prismjs
+    const loader = document.createElement("script");
+    loader.src =
+      "https://cdn.jsdelivr.net/npm/prismjs@v1.x/plugins/autoloader/prism-autoloader.min.js";
+    document.body.appendChild(loader);
+  };
+
+  $(document).ready(onLoaded);
+  $(document).on("DecryptEnded", onLoaded);
+
+  // ---------------------
+
+  $(document).ready(function () {
+    var QRBox = $("#QRBox");
+    var MainBox = $(".MainBox");
+    var BTCQR = $("#BTCQR");
+    var AliPayQR = $("#AliPayQR");
+    var WeChatQR = $("#WeChatQR");
     var currentQR;
 
-	  function showQR(QR) {
-		  $('#DonateText,#donateBox,#github').addClass('blur');
+    function showQR(QR) {
+      $("#DonateText,#donateBox,#github").addClass("blur");
       currentQR = QR;
-		  QRBox.fadeIn(300,function(argument) {
-			  QR.addClass('showQR');
-		  });
-	  }
+      QRBox.fadeIn(300, function (argument) {
+        QR.addClass("showQR");
+      });
+    }
 
-	  $('#donateBox>li').click(function(event) {
-		  var thisID	=	$(this).attr('id');
-		  if (thisID === 'BTC') {
-			  showQR(BTCQR);
-			  new ClipboardJS('#BTCBn');
-		  } else if (thisID === 'AliPay') {
-			  showQR(AliPayQR);
-		  } else if (thisID === 'WeChat') {
-			  showQR(WeChatQR);
-		  }
-	  });
+    $("#donateBox>li").click(function (event) {
+      var thisID = $(this).attr("id");
+      if (thisID === "BTC") {
+        showQR(BTCQR);
+        new ClipboardJS("#BTCBn");
+      } else if (thisID === "AliPay") {
+        showQR(AliPayQR);
+      } else if (thisID === "WeChat") {
+        showQR(WeChatQR);
+      }
+    });
 
-	  MainBox.click(function(event) {
-		  if (currentQR) currentQR.removeClass('showQR').addClass('hideQR');
-		  setTimeout (function(a) {
-			  QRBox.fadeOut(300,function(argument) {
-				  MainBox.removeClass('hideQR');
-			  });
-			  $('#DonateText,#donateBox,#github').removeClass('blur');
-		  },600);
-		});
+    MainBox.click(function (event) {
+      if (currentQR) currentQR.removeClass("showQR").addClass("hideQR");
+      setTimeout(function (a) {
+        QRBox.fadeOut(300, function (argument) {
+          MainBox.removeClass("hideQR");
+        });
+        $("#DonateText,#donateBox,#github").removeClass("blur");
+      }, 600);
+    });
 
-		// Hide mobile menu when click outside of the menu
-		$(document).click(function(event) {
-			const mainNavDisplay = $('.main-nav').css('display');
-			const mobileNav = $('.mobile-nav-link');
-			const menuBtn = $('#menu-button');
-			const mobileToggle = $('#mobile-menu-toggle');
-			const isClickedOutside = !$(event.target).is(mobileNav);
+    // Hide mobile menu when click outside of the menu
+    $(document).click(function (event) {
+      const mainNavDisplay = $(".main-nav").css("display");
+      const mobileNav = $(".mobile-nav-link");
+      const menuBtn = $("#menu-button");
+      const mobileToggle = $("#mobile-menu-toggle");
+      const isClickedOutside = !$(event.target).is(mobileNav);
 
-			if (mainNavDisplay !== 'none' || $(event.target).is(menuBtn) || $(event.target).is(mobileToggle) || mobileToggle.prop('checked') === false) return;
+      if (
+        mainNavDisplay !== "none" ||
+        $(event.target).is(menuBtn) ||
+        $(event.target).is(mobileToggle) ||
+        mobileToggle.prop("checked") === false
+      )
+        return;
 
-			if (isClickedOutside) {
-				mobileToggle.prop('checked', false);
-			}
-		});
+      if (isClickedOutside) {
+        mobileToggle.prop("checked", false);
+      }
+    });
   });
-})(jQuery)
+})(jQuery);
