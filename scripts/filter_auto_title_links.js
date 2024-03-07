9e8ae54f60a3e419ee1ce8ce2bc7f3303476fc64
pnck <pnckxx@pm.me>
2024-03-08 06:06:52 +0800

    chore: refine styles & auto title

--- scripts/filter_auto_title_links.js
+++ scripts/filter_auto_title_links.js
@@ -2,9 +2,14 @@
 // Add special link mark to link-only pages
 
 hexo.extend.filter.register("before_post_render", (data) => {
+  const tags = new Set(data.tags.data.map((tag) => tag.name));
+
   if (data.link && data.link.length > 0) {
     data.title = "⧉ " + data.title;
     data.portal = data.link;
+    if (!tags.has("Portal")) {
+      hexo.log.warn(`Post "${data.source}" is a link-only page but not tagged with "Portal"!`);
+    }
   }
   return data;
 });
