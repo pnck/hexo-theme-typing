2da157532979ebf11d1734acc0a0d51cd0842fbe
pnck <pnckxx@pm.me>
2024-03-11 06:54:00 +0800

    chore: move scripts away into main repo

--- scripts/filter_refine_br.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// Manually convert any break lines into <br> tags
-
-hexo.extend.filter.register("before_post_render", (data) => {
-  data.content = data.content.replace(/(^ *$\n){2}/gm, "\n<br>\n\n").replace(/(?<=\S)$/gm, " ".repeat(2));
-  return data;
-});
